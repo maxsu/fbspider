044d3fc8fe159b2b83fbccf4309b3c0bc8459179
unknown <Max@.(none)>
2010-03-24 01:49:21 -0400

    Messing about.

--- Facebook.hs
+++ Facebook.hs
@@ -3,59 +3,67 @@ import Network.URI
 import Network.Browser
 import Network.HTTP
 import Text.JSON
+import Text.Printf
 import Control.Monad
 import Control.Monad.State
 import Data.List hiding (insert)
 import System.Environment
 import Control.Concurrent.Chan
 import Control.Concurrent
-import Data.IntMap (IntMap)
-import qualified Data.IntMap as M
+import Data.IntMap (IntMap, empty, insert, size, (!))
 import System.IO
 import Numeric
 
 queryUri :: Int -> URI
-queryUri id = fromJust . parseURI $ "http://www.facebook.com/ajax/typeahead_friends.php?u="++show id++"&__a=1"
+queryUri uid = fromJust . parseURI $ "http://www.facebook.com/ajax/typeahead_friends.php?u="++show uid++"&__a=1"
 
-friends id = browse' $ do 
-  js <- fmap (drop (length "for (;;);") . rspBody . snd) . request . mkRequest GET . queryUri $ id
+friends :: Int -> IO [(Int, String)]
+friends uid = browse' $ do 
+  js <- fmap (drop (length "for (;;);") . rspBody . snd) . request . mkRequest GET . queryUri $ uid
   return $ case fromJson `fmap` decode js of
              (Ok (Just o)) -> o
-             _ -> []
+             _             -> []
 
-friendsAndFriends id = do 
-  c <- newChan
-  enqueue <- mkQueue 15 (round $ 10^6 * 0.5)
-  fs <- friends id
+friendsAndFriends :: Int -> IO (Graph String)
+friendsAndFriends uid = do 
+  c        <- newChan
+  enqueue  <- mkQueue 15 500000
+  fs       <- friends uid
   mapM_ (\(i,_) -> enqueue (writeChan c . (,) i =<< friends i)) fs
-  flip execStateT (Graph M.empty M.empty) $ do 
-                    addFriends (id,fs)
+  flip execStateT (Graph empty empty) $ do 
+                    addFriends (uid,fs)
                     mapM_ (\(i,_) -> addFriends =<< liftIO (readChan c)) (zip [0..] fs)
 
 addFriends (i,fs) = insertData fs >> insertEdges i (map fst fs) 
 
-data Graph a = Graph { labels :: !(IntMap a) , childrens :: !(IntMap [Int]) } deriving (Show,Read)
+data Graph a = Graph { labels :: !(IntMap a) , children :: !(IntMap [Int]) } deriving (Show,Read)
 
 modify' f = do s <- get
                let s' = f s               
                s' `seq` put s'
 
-insertData fs = modify' $ \g -> g { labels = foldl' (\m (n,l) -> M.insert n l m) (labels g) fs}
-insertEdges n ns = modify' $ \g -> g { childrens = M.insert n ns (childrens g)}
+insertData fs = modify' $ \g -> g { labels = foldl' (\m (n,l) -> insert n l m) (labels g) fs}
 
+insertEdges n ns = modify' $ \g -> g { children = insert n ns (children g)}
 
 browse' a = browse $ do
   setErrHandler $ const $ return ()
   setOutHandler $ const $ return ()
   setEventHandler Nothing
+  setEventHandler Nothing
+  setEventHandler Nothing
+  setEventHandler Nothing
   a
 
 field f (JSObject o) = lookup f $ fromJSObject o
 field _ _ = Nothing
+
 string (JSString s) = Just $ fromJSString s
 string _ = Nothing
+
 fbId (JSRational _ r) = return . truncate $ r
 fbId _ = Nothing
+
 fromJson x = do 
   JSArray xs <- field "friends" <=< field "payload" $ x
   mapM (liftM2 (liftM2 (,)) (fbId <=< field "i") (string <=< field "t")) xs 
@@ -70,16 +78,16 @@ mkQueue n d = do
   return $ writeChan c
 
 main = do 
-  [id] <- getArgs
-  let uid = read id
+  [idStr] <- getArgs
+  let uid = read idStr
   g <- friendsAndFriends uid
-  putStrLn "data"
+  putStrLn "Data:"
   print g
-  putStrLn "Summary.."
-  let friends = (childrens g M.! uid)
+  putStrLn "Summary:"
+  let friends = (children g ! uid)
       nfriends = length friends
-  putStrLn $ labels g M.! uid ++ " has " ++ show nfriends ++ " friends."
-  let average = (fromIntegral (sum . map (length . (childrens g M.!)) $ friends) / fromIntegral nfriends)
+  putStrLn $ labels g ! uid ++ " has " ++ show nfriends ++ " friends."
+  let average = (fromIntegral (sum . map (length . (children g !)) $ friends) / fromIntegral nfriends)
   putStrLn $ "Which have an average of " ++ showGFloat (Just 2) average "" ++ " friends."
-  putStrLn $ "For a total of " ++ show (M.size (labels g)) ++ " distinct users."
+  putStrLn $ "For a total of " ++ show (size (labels g)) ++ " distinct users."
 
